326864ef756c73c19fde67aa75d8318abb41ec74
Alan Laidlaw <alanlaidlaw@gmail.com>
2015-09-26 09:10:40 -0500

    added a background animation to the header to see if I like it. if I do, need to abstract this function.

--- src/js/cbpAnimatedHeader.js
+++ src/js/cbpAnimatedHeader.js
@@ -4,7 +4,7 @@
  *
  * Licensed under the MIT license.
  * http://www.opensource.org/licenses/mit-license.php
- * 
+ *
  * Copyright 2013, Codrops
  * http://www.codrops.com
  */
@@ -12,6 +12,7 @@ var cbpAnimatedHeader = (function() {
 
 	var docElem = document.documentElement,
 		header = document.querySelector( '.navbar-fixed-top' ),
+		marqueeMoon = document.querySelector( '.marqueeMoon'),
 		didScroll = false,
 		changeHeaderOn = 300;
 
@@ -27,10 +28,12 @@ var cbpAnimatedHeader = (function() {
 	function scrollPage() {
 		var sy = scrollY();
 		if ( sy >= changeHeaderOn ) {
-			classie.add( header, 'navbar-shrink' );
+			classie.add( header, 'navbar-shrink' ),
+			classie.add( marqueeMoon, 'midnight' );
 		}
 		else {
-			classie.remove( header, 'navbar-shrink' );
+			classie.remove( header, 'navbar-shrink' ),
+			classie.remove( marqueeMoon, 'midnight' );
 		}
 		didScroll = false;
 	}
